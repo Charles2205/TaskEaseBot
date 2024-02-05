d16646080d85b3a57d9274e1d71d7d92bc7d73e9
Charles2205 <cewudzie0@gmail.com>
2024-02-05 12:22:02 +0000

    Update commands

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const TelegramBot = require('node-telegram-bot-api');
-require('dotenv').config()
-const TOKEN = process.env.BOT_KEY
+require('dotenv').config();
+const TOKEN = process.env.BOT_KEY;
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const messageText = msg.text;
-    if (messageText === '/start') {
-        bot.sendMessage(chatId, `Welcome to the TaskEaseBot ${msg}`);
 
-      }
-      console.log(msg);
-  });
+    if (messageText === '/start') {
+        bot.sendMessage(chatId,'Welcome to TaskEaseBot Youtube Downloader \n Paste the Youtube Link in here ')
+    }
+    const fetchData =async(userInput)=>{
+        let getuserInput = 'Your name is '
+        console.log(getuserInput+userInput);
+        
+    }
+    fetchData(msg.text)
+})
